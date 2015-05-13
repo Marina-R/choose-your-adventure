382d867a0e505c822d56987e8c53ac6a257680ce
maryna <luv_this_world@hotmail.com>
2015-05-12 23:33:23 -0500

    My adventure

--- dist/game.js
+++ dist/game.js
@@ -1,18 +1,10 @@
 function* chooseYourAdventure() {
-	// console.log(yield adventure.say('Hey there!'));
-	// console.log(yield adventure.say('Woah', 'This is cool'));
-	// console.log(yield adventure.ask('Woah', 'This is cool'));
-	// console.log(yield adventure.choose('Woah', 'This is cool', 'Hello world'));
-	// console.log(yield adventure.say('That\'s all folks!', 'THE END'));
 
-	
 	var name = yield adventure.ask ('Hey you! What is your name?');
 	yield adventure.say('Welcome to my game, ' + name + '!');
 	var game = yield adventure.choose ('Let\'s play "Rock Paper Scissor" game with me!', 'Let\'s do it!', 'Maybe some other time.');
 	var choice = Math.floor(Math.random()*3)+1;  
-	// var rock = 1;
-	// var scissor = 2;
-	// var paper = 3;
+
 	if (game=='Let\'s do it!') {
 		yield adventure.say ('Yeah! Make yourself comfortable.');
 	} else {
